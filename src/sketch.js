29a62a360274ab3522273c1a6d9facb72b57190a
github username <itsDario1@gmail.com>
2019-05-06 17:41:26 -0400

    adding string testing against array

--- src/sketch.js
+++ src/sketch.js
@@ -1,11 +1,19 @@
+var lastEnteredWord = []
+var wordArr = ['test', 'word']
+var success = ''
+
 function setup() {
   // put setup code here
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   // put drawing code here
+  background(255)
+  // fill(000000)
   ellipse(50, 50, 50, 50)
-
+  text((lastEnteredWord.join('')), 10, 30);
+  text((success), 10, 30);
 }
 
 // function key_pressed() {
@@ -13,6 +21,15 @@ function draw() {
 // }
 
 function keyPressed() {
-  console.log(key);
+  lastEnteredWord.push(key)
+  if (key == ' ' || key === 'Enter') {
+    // console.log('space');
+    lastEnteredWord = []
+  }
+  console.log(wordArr.indexOf(lastEnteredWord.join('')));
 
+  // if ((searchStringInArray(lastEnteredWord.join(''), wordArr) > -1) && ) {
+  // success = 'Success'
+  // }
+  // console.log(lastEnteredWord.join(''));
 }
