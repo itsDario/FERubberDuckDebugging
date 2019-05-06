6fbc90b5e0de5061ed38bdc48ca305ce82bea4aa
github username <itsDario1@gmail.com>
2019-05-06 15:07:30 -0400

    added keyboard input

--- src/sketch.js
+++ src/sketch.js
@@ -1,9 +1,18 @@
 function setup() {
   // put setup code here
-  // canvas.width = 100%
 }
 
 function draw() {
   // put drawing code here
   ellipse(50, 50, 50, 50)
+
+}
+
+// function key_pressed() {
+//   console.log('keypress', key);
+// }
+
+function keyPressed() {
+  console.log(key);
+
 }
