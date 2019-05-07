4e862d55ab9d9de1b917daf3287e8b7760d26f68
github username <itsDario1@gmail.com>
2019-05-07 08:37:57 -0400

    made background blue

--- src/sketch.js
+++ src/sketch.js
@@ -1,35 +1,44 @@
-var lastEnteredWord = []
-var wordArr = ['test', 'word']
-var success = ''
+let lastEnteredWord = []
+let wordArr = ['test', 'word']
+let success = ''
 
 function setup() {
   // put setup code here
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth - 10, windowHeight - 90);
+  textAlign(CENTER);
 }
 
 function draw() {
   // put drawing code here
-  background(255)
-  // fill(000000)
-  ellipse(50, 50, 50, 50)
-  text((lastEnteredWord.join('')), 10, 30);
-  text((success), 10, 30);
+  background(80, 10, 190) //white background 
+  ellipse(width / 2, height / 2, 50, 50) //testing circle
+  text((lastEnteredWord.join('')), width / 2, height / 2); //render words by combinig letter array
+  displayCurrentWords()
 }
 
-// function key_pressed() {
-//   console.log('keypress', key);
-// }
+function displayCurrentWords() {
+
+  text(wordArr.join(', '), width / 2, (height / 2) + 50);
+}
 
 function keyPressed() {
-  lastEnteredWord.push(key)
-  if (key == ' ' || key === 'Enter') {
+  // console.log(key.length);
+  if (key == 'Backspace') {
+    lastEnteredWord.pop()
+  }
+  if (key.length < 2) {
+    lastEnteredWord.push(key)
+  }
+  if (key === 'Enter') {
     // console.log('space');
     lastEnteredWord = []
   }
-  console.log(wordArr.indexOf(lastEnteredWord.join('')));
 
-  // if ((searchStringInArray(lastEnteredWord.join(''), wordArr) > -1) && ) {
-  // success = 'Success'
-  // }
-  // console.log(lastEnteredWord.join(''));
+  // console.log(wordArr.indexOf(lastEnteredWord.join('')));
+  if (wordArr.indexOf(lastEnteredWord.join('')) === -1) {
+    fill(255, 204, 0);
+  } else {
+    noFill();
+  }
+  fill(0)
 }
