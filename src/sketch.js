76256d5e2b7cdd4ebea0d6f61a9047ca1873dcf6
github username <itsDario1@gmail.com>
2019-05-07 10:17:59 -0400

    refactor of duck display array and set highlight of current correctly typed out duck

--- src/sketch.js
+++ src/sketch.js
@@ -1,25 +1,46 @@
 let lastEnteredWord = []
-let wordArr = ['test', 'word']
 let success = ''
+let ducksArray
 
 function setup() {
   // put setup code here
   createCanvas(windowWidth - 10, windowHeight - 90);
   fill(255, 0, 0)
   textAlign(CENTER);
+  ducksArray = {
+    currentWords: ['test', 'word', '', '', '', ''],
+    xLocations: [(width / 6) * 1.5, (width / 6) * 3, (width / 6) * 4.5, (width / 6) * 1.5, (width / 6) * 3, (width / 6) * 4.5],
+    yLocations: [(height / 4) * 1, (height / 4) * 1, (height / 4) * 1, (height / 4) * 2.5, (height / 4) * 2.5, 50, (height / 4) * 2.5]
+  }
 }
 
 function draw() {
   // put drawing code here
   background(80, 10, 190) //white background 
-  ellipse(width / 2, height / 4, 50, 50) //testing circle
   text((lastEnteredWord.join('')), width / 2, height / 2); //render words by combinig letter array
   displayCurrentWords()
+  drawDucks()
+}
+
+function drawDucks() {
+  for (let i = 0; i < ducksArray["currentWords"].length; i++) {
+    if (ducksArray["currentWords"][i].length > 0) {
+
+      if (lastEnteredWord.join('') == ducksArray["currentWords"][i]) {
+        fill(255, 204, 0);
+      }
+
+      ellipse(ducksArray["xLocations"][i], ducksArray["yLocations"][i], 50, 50)
+      text(ducksArray["currentWords"][i], ducksArray["xLocations"][i], ducksArray["yLocations"][i] + 50);
+    }
+
+    fill(255, 0, 0)
+  }
 }
 
 function displayCurrentWords() {
 
-  text(wordArr.join(', '), width / 2, (height / 2) + 50);
+  text(ducksArray["currentWords"].join(', '), width / 2, (height / 2) + 50);
 }
 
 function keyPressed() {
@@ -35,10 +56,10 @@ function keyPressed() {
     lastEnteredWord = []
   }
 
-  console.log(wordArr.indexOf(lastEnteredWord.join('')));
-  if (wordArr.indexOf(lastEnteredWord.join('')) > -1) {
-    fill(255, 204, 0);
-  } else {
-    fill(255, 0, 0)
-  }
+  console.log(ducksArray["currentWords"].indexOf(lastEnteredWord.join('')));
+  // if (ducksArray["currentWords"].indexOf(lastEnteredWord.join('')) > -1) {
+  //   fill(255, 204, 0);
+  // } else {
+  //   fill(255, 0, 0)
+  // }
 }
